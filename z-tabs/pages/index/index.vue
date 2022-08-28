29c2ed01ce32ad726e32b91735be1d1224dead35
SmileZXLee <admin@zxlee.cn>
2022-08-28 15:39:50 +0800

    v0.1.4

--- z-tabs/pages/index/index.vue
+++ z-tabs/pages/index/index.vue
@@ -1,5 +1,6 @@
 <template>
 	<view class="content">
+		<!-- 基本使用 -->
 		<view class="group">
 			<text class="group-title">
 				基本使用
@@ -7,30 +8,39 @@
 			<z-tabs :list="list"></z-tabs>
 		</view>
 		
+		<!-- 自定义高亮颜色 -->
 		<view class="group">
 			<text class="group-title">
 				自定义高亮颜色
 			</text>	
 			<z-tabs :list="list" active-color="orange"></z-tabs>
 		</view>
+		
+		<!-- 自定义高亮style -->
 		<view class="group">
 			<text class="group-title">
 				自定义高亮style
 			</text>	
 			<z-tabs :list="list" :active-style="{'font-size':'40rpx','font-weight':'bold'}"></z-tabs>
 		</view>
+		
+		<!-- 自定义初始选中tab -->
 		<view class="group">
 			<text class="group-title">
 				自定义初始选中tab
 			</text>	
 			<z-tabs :list="list" :current="5"></z-tabs>
 		</view>
+		
+		<!-- 显示徽标数 -->
 		<view class="group">
 			<text class="group-title">
 				显示徽标数
 			</text>	
 			<z-tabs :list="badgeList"></z-tabs>
 		</view>
+		
+		<!-- 自定义右侧插槽 -->
 		<view class="group">
 			<text class="group-title">
 				自定义右侧插槽
@@ -41,6 +51,23 @@
 				</template>
 			</z-tabs>
 		</view>
+		
+		<!-- 与swiper联动 -->
+		<view class="group">
+			<text class="group-title">
+				与swiper联动
+			</text>	
+			<z-tabs ref="tabs" :current="swiperCurrent" :list="swiperList" @change="tabsChange"></z-tabs>
+			<swiper class="swiper" :current="swiperCurrent" @transition="swiperTransition" @animationfinish="swiperAnimationfinish">
+			    <swiper-item v-for="(item, index) in swiperList" :key="index">
+					<view class="swiper-item-view" :style="{backgroundColor: item.color}">
+						{{item.name}}
+					</view>
+			    </swiper-item>
+			</swiper>
+		</view>
+		
+		
 		<!-- #ifdef APP-PLUS -->
 		<view class="to-nvue-btn" @click="gotoNvue">
 			查看nvue-demo
@@ -59,7 +86,9 @@
 		data() {
 			return {
 				list: [],
-				badgeList: []
+				badgeList: [],
+				swiperList: [],
+				swiperCurrent: 0
 			}
 		},
 		onLoad() {
@@ -87,18 +116,56 @@
 				}
 			}
 			this.badgeList = badgeList;
+			
+			//与swiper联动数组
+			const swiperList = [];
+			for(let i = 0;i < 4;i++) {
+				swiperList.push({
+					name: 'tab' + (i + 1),
+					color: this._getRandomColor()
+				});
+			}
+			this.swiperList = swiperList;
 		},
 		methods: {
 			gotoNvue(){
 				uni.navigateTo({
 					url: '/pages/index-nvue/index-nvue'
 				})
+			},
+			
+			//tabs通知swiper切换
+			tabsChange(index) {
+				this.swiperCurrent = index;
+			},
+			//swiper滑动中
+			swiperTransition(e) {
+				this.$refs.tabs.setDx(e.detail.dx);
+			},
+			//swiper滑动结束
+			swiperAnimationfinish(e) {
+				this.swiperCurrent = e.detail.current;
+				this.$refs.tabs.unlockDx();
+			},
+			
+			// 生成随机颜色
+			_getRandomColor() {
+				const rgb = [];
+				for (let i = 0; i < 3; ++i) {
+					let color = Math.floor(Math.random() * 256).toString(16)
+					color = color.length == 1 ? '0' + color : color
+					rgb.push(color)
+				}
+				return '#' + rgb.join('');
 			}
 		}
 	}
 </script>
 
 <style>
+	.content{
+		padding-bottom: 20rpx;
+	}
 	.group{
 		margin-top: 30rpx;
 	}
@@ -106,6 +173,18 @@
 		padding-left: 20rpx;
 		font-size: 26rpx;
 		color: #aaaaaa;
+	}
+	.swiper{
+		height: 300rpx;
+	}
+	.swiper-item-view{
+		background-color: #007AFF;
+		height: 300rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: white;
+		font-size: 50rpx;
 	}
 	.to-nvue-btn{
 		background-color: #007AFF;
